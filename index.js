9041d536d68b220897b639856263cd46938583f5
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-08-06 14:39:52 -0700

    add pictures

--- index.js
+++ index.js
@@ -12,21 +12,69 @@ app.get("/", (req,res) => {
     res.render("home");
 })
 
+app.get("/prehistoric_creatures", (req,res) => {
+    let creatures = fs.readFileSync("./prehistoric_creatures.json");
+    let creatureData = JSON.parse(creatures);
+    let creatureFilter = req.query.creatureFilter;
+    if (creatureFilter) {
+        creatureData = creatureData.filter((creature) => {
+            return creature.type.toLowerCase() === creatureFilter.toLowerCase();
+        })
+    }
+    
+    //render our creatures index page
+    res.render("prehistoric_creatures/index", {myCreatures: creatureData});
+})
+
+app.get("/prehistoric_creatures/new", (req, res) => {
+    res.render("prehistoric_creatures/new");
+
+})
+
+app.get("/prehistoric_creatures/:id", (req, res) => {
+    let creatures = fs.readFileSync("./prehistoric_creatures.json");
+    let creatureData = JSON.parse(creatures);
+    let creatureIndex = parseInt(req.params.id);
+    res.render("prehistoric_creatures/show", {myCreatures: creatureData[creatureIndex]})
+})
+
+app.post("/prehistoric_creatures", (req,res) => {
+    let creatures = fs.readFileSync("./dinosaurs.json");
+    let creatureData = JSON.parse(creatures); //dino data is an array
+    //push new dino to the array
+    creatureData.push(req.body);
+    //convert dinodata back to json and write to dinosaurs.json file
+    fs.writeFileSync("./prehistoric_creatures.json", JSON.stringify(creatureData)); //does the opposite of JSON parse
+    // redirect to the index GET route
+    res.redirect("/prehistoric_creatures");
+})
+
+//DINOSAURS IS WORKING!!!
+
 //index route
 app.get("/dinosaurs", (req,res) => {
     //get the json from dinosaurs.json
     let dinosaurs = fs.readFileSync("./dinosaurs.json");
     //convert the json to javascript
     let dinoData = JSON.parse(dinosaurs);
-    console.log(dinoData);
+    let nameFilter = req.query.nameFilter;
+    //console.log(nameFilter);
+    //console.log(req.query);
+    //keep in dinodata any dinos whos name matches the nameFilter the user searched for
+    //this is the part that makes the list list disappear upon searching for name of dino
+    if (nameFilter) {
+        dinoData = dinoData.filter((dino) => {
+            return dino.name.toLowerCase() === nameFilter.toLowerCase();
+        })
+    }
     //render our dino index page and pass it in the dino data as myDinos
     res.render("dinosaurs/index", {myDinos: dinoData});
 })
 
+
 //get the new dino form
 app.get("/dinosaurs/new", (req, res) => {
     res.render("dinosaurs/new");
-
 })
 
 //show route
